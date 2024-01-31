2ffd31eeb2537ee02d1ee1d0a49c08017dc761a3
fchabrun <floris.chabrun@chu-angers.fr>
2024-01-31 17:29:07 -0500

    added console color_print

---
+++ python/console.py
@@ -0,0 +1,19 @@
+_CONSOLE_COLOR = {
+    'blue': '\033[94m',
+    'grey': '\033[90m',
+    'yellow': '\033[93m',
+    'black': '\033[90m',
+    'cyan': '\033[96m',
+    'green': '\033[92m',
+    'magenta': '\033[95m',
+    'white': '\033[97m',
+    'red': '\033[91m',
+    'default': '\033[0m',
+}
+
+
+def color_print(*args, color, **kwargs):
+    print(_CONSOLE_COLOR.get(color.lower(), _CONSOLE_COLOR['default']), end="")
+    print(*args, **kwargs)
+    print(_CONSOLE_COLOR['default'], end="")
+
